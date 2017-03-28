920c2a587101c625c01f3183f2a797184dc4a5fb
Spidergears <findspidergears@gmail.com>
2017-03-29 00:35:24 +0530

    Added waybill API

--- lib/delhivery/services/way_bill_service.rb
+++ lib/delhivery/services/way_bill_service.rb
@@ -1,63 +1,18 @@
 module Delhivery
   class WayBillService < BaseService
     class << self
-      # def bulk_next(client_name:, count=1)
-      #   connection.get(path('bulk'),
-      #     {
-      #       action: 'next',
-      #       count: count,
-      #       cl: client_name
-      #     }
-      #   )
-      # end
+      def bulk_fetch(count=1)
+        connection.get(path('bulk'), { count: count } )
+      end
 
-      # def bulk_fetch(client_name:, waybill_numbers:, count=1)
-      #   connection.get(path('bulk'),
-      #     {
-      #       action: 'fetch',
-      #       count: count,
-      #       cl: client_name,
-      #       wbn: waybill_numbers.join(',')
-      #     }
-      #   )
-      # end
+      def fetch
+        connection.get(path('fetch'))
+      end
 
-      # def next(client_name:, count=1)
-      #   connection.get(path('bulk'),
-      #     {
-      #       action: 'next',
-      #       count: count,
-      #       cl: client_name
-      #     }
-      #   )
-      # end
-
-      # def bulk_fetch(client_name:, waybill_numbers:, count=1)
-      #   connection.get(path('bulk'),
-      #     {
-      #       action: 'fetch',
-      #       count: count,
-      #       cl: client_name,
-      #       wbn: waybill_numbers.join(',')
-      #     }
-      #   )
-      # end
-
-      # private
-      # def path(action)
-      #   "/waybill/api/#{action}/json/"
-      # end
-
-      # def make_request(action:, client_name: , count: , waybill_numbers: )
-      #   connection.get(path(action),
-      #     {
-      #       action: 'fetch',
-      #       count: count,
-      #       cl: client_name,
-      #       wbn: waybill_numbers.join(',')
-      #     }
-      #   )
-      # end
+      private
+      def path(action)
+        "/waybill/api/#{action}/json/"
+      end
     end
   end
 end
